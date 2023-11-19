db9a56df2ebe73ec9a91dd9c1faf39f586d96a04
Aurélien Fossard <aurelien.fossard2023@campus-eni.fr>
2023-11-19 11:31:34 +0100

    feat: Add HTTP methods

---
+++ src/data/http.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { IRoomResponse } from "models/api";
+import * as process from "process";
+
+const baseUrl = process.env.REACT_APP_SERVER;
+
+export const createRoom = () =>
+  axios.post<IRoomResponse>(`${baseUrl}/rooms`, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+export const retrieveRoom = (room: string) =>
+  axios.get<IRoomResponse>(`${baseUrl}/rooms?code=${room}`, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
