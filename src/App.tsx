2d91b1558e4068538903cd80f74f20ac6cd1a415
Aurélien Fossard <aurelien.fossard2023@campus-eni.fr>
2023-11-19 11:34:10 +0100

    feat: Add routes and plug Home & Room to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "views/pages/Home";
+import RoomPage from "views/pages/Room";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+import { createContext, useState } from "react";
+import { IUser } from "models/interfaces";
+import { AvatarName } from "constants/avatars";
+import { getRandomName } from "utils/userDataRandomizer";
+
+const defaultUserData: IUser = {
+  username: getRandomName(),
+  avatar: AvatarName.RED_PANDA,
+  id: "",
+};
+
+export const UserCtx = createContext<{
+  user: IUser;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+}>({ user: defaultUserData, setUser: () => {} });
+
 function App() {
+  const [user, setUser] = useState<IUser>(defaultUserData);
+
   return (
-      <div></div>
+      <>
+        <CssBaseline />
+        <GlobalStyles
+            styles={{
+              body: {
+                background:
+                    "radial-gradient(circle, #dfdfdf 10%, #6b5b95 50%, black)",
+              },
+            }}
+        />
+        <UserCtx.Provider value={{ user, setUser }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/:id" element={<RoomPage />} />
+            </Routes>
+          </BrowserRouter>
+        </UserCtx.Provider>
+      </>
   );
 }
 
